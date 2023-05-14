b4a2ac90adce2de90feefa298fd154f8004b5419
Bryant Whitsett <bryantwhitsett71@gmail.com>
2023-05-14 16:29:04 -0400

    Added files to github

---
+++ Console App/Program.cs
@@ -0,0 +1,16 @@
+﻿// Declaring Variables
+
+string name = "Bryant";
+int age = 29;
+char middleInitial = 'K';
+bool isOver18 = true;
+double currentTemp = 78;
+decimal price = 792;
+
+// Initializing Variables by giving the name a value
+Console.WriteLine($"Name: {name}");
+Console.WriteLine($"Age: {age}");
+Console.WriteLine($"Middle Initial: {middleInitial}");
+Console.WriteLine($"IsOver18: {isOver18}");
+Console.WriteLine($"CurrentTemp: {currentTemp}");
+Console.WriteLine($"Price: {price}");
