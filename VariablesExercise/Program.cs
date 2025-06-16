4beef74e439b3809465cafb20cc81e2c680ddbed
bryantwhitsett <bryantwhitsett71@gmail.com>
2025-06-15 20:17:28 -0400

    Declared & initialized all my variables using string interpolation.

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,34 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string favoriteVacation = "Hawaii";
+            int timesVisited = 2;
+            char planToGOx = '4';
+            double vacationMoney = 3000;
+            decimal gdp = 115627000000m;
+
+            Console.WriteLine($"My favorite place to vacation is {favoriteVacation}. I've only visited {timesVisited}" +
+                              $" times but I'm going at least {planToGOx} more you feel me!");
+
+            Console.WriteLine($" Is it true that Hawaii has high cost of living? ");
+            
+            bool userAnswer = bool.Parse(Console.ReadLine());
+
+            if (userAnswer == true)
+            {
+                Console.WriteLine($" Well it's a secluded island that has to pay for exports on" +
+                                  $" mostly everything and they rely heavily opn tourism. That's why I'm trying to" +
+                                  $" bring at least {vacationMoney} maybe a little more. Man I just looked them up" +
+                                  $" on-line they has one of the lowest gpd in the country, only {gdp}.");
+            }
+
+            if (userAnswer == false)
+            {
+                Console.WriteLine("Well I've heard otherwise but I'm going there to see for myself!");
+            }
+
+
+
         }
     }
 }
